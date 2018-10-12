818dd7a5b3ff30cb8c9608d56cb1107056ae3bb0
Mário Gil <mg_mesquita@hotmail.com>
2018-10-12 18:51:11 +0100

    Finished Lab 2

--- Lab 2/lab2.c
+++ Lab 2/lab2.c
@@ -2,9 +2,12 @@
 
 #include <lcom/lab2.h>
 #include <lcom/timer.h>
+#include "i8254.h"
 
 #include <stdbool.h>
 #include <stdint.h>
+
+extern unsigned int timerIntCounter;
 
 int main(int argc, char *argv[]) {
     // sets the language of LCF messages (can be either EN-US or PT-PT)
@@ -58,11 +61,55 @@ int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
     return 0;
 }
 
-int(timer_test_int)(uint8_t UNUSED(time)) {
-    /* To be completed by the students */
-    printf("%s is not yet implemented!\n", __func__);
+int(timer_test_int)(uint8_t time) {
 
-    return 1;
+
+    int ipc_status;
+    message msg;
+
+    //Holds results
+    uint32_t r;
+
+    uint8_t bit_no = 0;
+    if((r = timer_subscribe_int(&bit_no)) != OK){
+        printf("(%s) Couldnt subscribe interrupt\n", __func__);
+        return 1;
+    }
+
+    uint32_t irq_set = BIT(bit_no);
+
+    while(time) {
+        /* Get a request message.  */
+        if( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) {
+            printf("driver_receive failed with: %d", r);
+            continue;
+        }
+
+        if (is_ipc_notify(ipc_status)) { /* received notification */
+            switch (_ENDPOINT_P(msg.m_source)) {
+                case HARDWARE: /* hardware interrupt notification */
+                if ( msg.m_notify.interrupts & irq_set) { /* subscribed interrupt */
+                    timer_int_handler();
+
+                    if(timerIntCounter == 0){
+                        //Decrement the time
+                        time--;
+                        timer_print_elapsed_time();
+                    }
+                    
+                }
+                break;
+            default:
+                break; /* no other notifications expected: do nothing */
+            }
+        }
+        else { /* received a standard message, not a notification */
+        /* no standard messages expected: do nothing */
+        }
+    }
+
+    timer_unsubscribe_int();
+    return 0;
 }
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
@@ -70,7 +117,7 @@ int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
     if(lsb == NULL)
         return 1;
     *lsb = (uint8_t) (0x00ff & val);
-    return 0;
+    return OK;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
@@ -78,5 +125,5 @@ int(util_get_MSB)(uint16_t val, uint8_t *msb) {
     if(msb == NULL)
         return 1;
     *msb = (uint8_t) ((0xff00 & val)>>8);
-    return 0;
+    return OK;
 }
--- Lab 2/timer.c
+++ Lab 2/timer.c
@@ -30,8 +30,7 @@
     if(timer_id > 2) { \
         printf("(%s) Timer range must be between 0 and 2! %d given\n", __func__, timer_id); \
         return TIMER_OUT_RANGE; \
-    } \
-    (void)0
+    }
 
 /*
  * Reason: Code reusability
@@ -42,8 +41,7 @@
     if((res = sys_outb(arg1, arg2)) != OK) { \
         printf("(%s) sys_outb failed (%s): %d\n", __func__, message, res); \
         return TIMER_OUTB_FAILED; \
-    } \
-    (void)0
+    }
 
 /*
  * Reason: Code reusability
@@ -54,8 +52,11 @@
     if((res = sys_inb(arg1, arg2)) != OK) { \
         printf("(%s) sys_inb failed (%s): %d\n", __func__, message, res); \
         return TIMER_INB_FAILED; \
-    } \
-    (void)0
+    }
+
+#define UINT16_T_MAX (uint16_t)0xFFFF
+#define TIMER_MAX_FREQ TIMER_FREQ
+#define TIMER_MIN_FREQ (uint16_t)TIMER_FREQ/UINT16_T_MAX + (((uint16_t)TIMER_FREQ % UINT16_T_MAX) ? 1 : 0)
 
 typedef enum _timer_status{
     TIMER_OK = OK,
@@ -85,8 +86,8 @@ typedef enum _timer_status{
 } timer_status;
 
 
-int timerHookID = 1;
-uint8_t timerIntCounter = 0;
+int timerHookID = 31;
+unsigned int timerIntCounter = 0;
 
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
 
@@ -101,7 +102,7 @@ int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
      */
 
     //When freq > TIMER_FREQ then TIMER_FREQ/freq = 0 because we're performing integer division, lets avoid that case
-    if (freq > TIMER_FREQ){
+    if (freq > TIMER_MAX_FREQ){
         printf("(%s) freq is higher than TIMER_FREQ\n", __func__);
         return TIMER_FREQ_TOO_HIGH;
     }
@@ -109,25 +110,25 @@ int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
     //LSB and MSB are 8bits each, so that means the divisor is 16 bit, the maximum value that an unsigned 16 bit int
     //can hold is 2^16 - 1 = 65535. TIMER_FREQ / 65535 = 18.2067902647. Since we're performing integer division
     //the minimum is obtained by rounding up, 19.
-    if (freq < 19){
+    if (freq < TIMER_MIN_FREQ){
         printf("(%s) freq cant be lower than 19", __func__);
         return TIMER_FREQ_TOO_LOW;
     }
 
     /*
      * Since we're changing the frequency, we must make sure the counting mode
-     * and the counting base are preserved, thus we'are preserving the last 4 bits
+     * and the counting base are preserved, thus we are preserving the last 4 bits
      * of the previous timer configuration
      */
 
     // Get previous timer configuration
     uint8_t previousConfig;
-    timer_get_conf(timer, &previousConfig);
+    if( (res = timer_get_conf(timer, &previousConfig)) != TIMER_OK)
+        return res;
 
     //Build the control word, maintaining the 4 last bits
     uint8_t controlWord = 0;
     controlWord |= TIMER_SELN(timer) | TIMER_LSB_MSB | (previousConfig & (BIT(3) | BIT(2) | BIT(1) | BIT(0)));
-
 
     // Write control word to control register
     SYS_OUTB_SAFE(res, "writting control word", TIMER_CTRL, controlWord);
@@ -135,6 +136,11 @@ int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
     // Calculate initial value to be loaded to get the given frequency
     uint16_t divisor = TIMER_FREQ / freq;
     uint8_t lsb = 0 , msb = 0;
+    
+    /*
+     * Since the timer uses two 8 bit registers, we need to separate the 16 bit
+     * value in two, thus the separation in LSB and MSB
+     */
 
     if(util_get_LSB(divisor, &lsb) != OK){
         printf("(%s) util_get_LSB: failed reading lsb\n", __func__);
@@ -147,8 +153,8 @@ int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
     }
 
     uint8_t selectedTimer = TIMER_N(timer);
-
-    // Load the 2 byte value to the timer; In 2 steps because the counter uses two 1 byte registers.
+    
+    // Write the lsb and msb to the respective timer registers
     SYS_OUTB_SAFE(res, "writting lsb", selectedTimer, lsb);
     SYS_OUTB_SAFE(res, "writting msb", selectedTimer, msb);
 
@@ -166,30 +172,21 @@ int (timer_subscribe_int)(uint8_t *bit_no) {
     *bit_no = timerHookID;
 
     // Subscribe a notification on every interrupt in the specified timer's IRQ line
-    if (sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &timerHookID) != OK) {
+    if (sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &timerHookID) != TIMER_OK) {
         printf("(%s) sys_irqsetpolicy: failed subscribing timer interrupts\n", __func__);
         return TIMER_INT_SUB_FAILED;
     }
-    
-    // Enable interrupts on the IRQ line associated with the hook id
-    if (sys_irqenable(&timerHookID) != OK) {
-        printf("(%s) sys_irqenable: failed enabling timer interrupts\n", __func__);
-        return TIMER_INT_SUB_FAILED;
-    }
 
+    // Since we use the IRQ_REENABLE policy, we do not have to use sys_irqenable
     return TIMER_OK;
 }
 
 int (timer_unsubscribe_int)() {
     
-    // Disable interrupts on the IRQ Line associated with the hook id
-    if (sys_irqdisable(&timerHookID) != OK) {
-        printf("(%s) sys_irqdisable: failed disabling timer interrupts\n", __func__);
-        return TIMER_INT_UNSUB_FAILED;
-    }
+    // Since we use the IRQ_REENABLE policy, we do not have to use sys_irqdisable
 
     // Unsubscribe the interrupt notifications associated with hook id
-    if (sys_irqrmpolicy(&timerHookID) != OK) {
+    if (sys_irqrmpolicy(&timerHookID) != TIMER_OK) {
         printf("(%s) sys_irqrmpolicy: failed unsubscribing timer interrupts\n", __func__);
         return TIMER_INT_UNSUB_FAILED;
     }
@@ -197,9 +194,15 @@ int (timer_unsubscribe_int)() {
     return TIMER_OK;
 }
 
+/*
+ * Although the current implementation works with uint8_t which max value is 255
+ * timerIntCounter can easily hold 60 times that. The problem arises when bigger
+ * values are used, they can cause a overflow, thus by limiting the counter to 0-59
+ * we guarantee it works for any period of time
+ */
 void (timer_int_handler)() {
     //Increment the timer interruption counter global variable
-    timerIntCounter++;
+    timerIntCounter = (timerIntCounter+1) % 60;
 }
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
@@ -225,7 +228,8 @@ int (timer_get_conf)(uint8_t timer, uint8_t *st) {
     // Read configuration from given timer and write it to the location specified
     uint32_t status = 0;
     SYS_INB_SAFE(res, "reading status", TIMER_N(timer), &status);
-
+    
+    // Return the read configuration via the argument 
     *st = (uint8_t) status;
 
     return TIMER_OK;
@@ -235,6 +239,8 @@ int (timer_display_conf)(uint8_t timer, uint8_t st,
                         enum timer_status_field field) {
     	
     CHECK_TIMER_RANGE(timer);
+
+    //Used to hold the result of any operation
     uint8_t res = 0;
 
     union timer_status_field_val timerStatus;
@@ -256,7 +262,7 @@ int (timer_display_conf)(uint8_t timer, uint8_t st,
             break;
     }
 
-    if((res = timer_print_config(timer, field, timerStatus)) != OK){
+    if((res = timer_print_config(timer, field, timerStatus)) != TIMER_OK){
         printf("(%s) timer_print_config returned: %d\n", __func__, res);
         return TIMER_LCF_ERROR;
     }
